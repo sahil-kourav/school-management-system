88c61a89e403601197dcb678f7ebead65cd06f87
Sahil Kourav <sahil-kourav@users.noreply.github.com>
2025-02-20 13:53:12 +0530

    vercel related changes

--- backend/index.js
+++ backend/index.js
@@ -44,12 +44,11 @@ dotenv.config();
 const app = express();
 const Routes = require("./routes/route.js");
 
-const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(express.json({ limit: '10mb' }));
 app.use(cors());
+app.use(express.json({ limit: '10mb' }));
 
 app.use('/', Routes);
 
@@ -57,6 +56,14 @@ app.get('/', (req,res)=>{
     res.send("hello");
 })
 
-app.listen(PORT, () => {
-    console.log(`Server started at port no. ${PORT}`);
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server started at port no. ${PORT}`);
+// });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
